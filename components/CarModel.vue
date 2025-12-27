25008e4fd4569d970634132706ce2fb90f5024d4
user <mail@hanymi.ru>
2025-12-27 10:58:26 +0300

    add tariff 2

--- components/CarModel.vue
+++ components/CarModel.vue
@@ -1,28 +1,38 @@
 <template>
-  <div class="carsList-item carsList-ite">
+  <div :class="['carsList-item', { 'is-bought-out': car.is_bought_out === 1 }]">
+    <!-- Ссылка активна только если машина НЕ выкуплена -->
     <nuxt-link
-      v-if="link"
-      :to="`/${car.brand.slug}/${car.slug}`"
-      :class="[
+        v-if="link && car.is_bought_out !== 1"
+        :to="`/${car.brand.slug}/${car.slug}`"
+        :class="[
         'carsList-itemContent',
         [carousel ? 'carsList-itemContent-agile' : ''],
       ]"
     >
       <CarModelInfo :car="car" :carousel="carousel" />
-      <div class="carsList-itemHover" v-if="link">
+      <div class="carsList-itemHover">
         <div class="carsList-itemButtonContainer">
           <div class="carsList-itemButton">Подробнее</div>
         </div>
       </div>
     </nuxt-link>
+
+    <!-- Блок без ссылки: если машина выкуплена -->
     <div
-      v-if="!link"
-      :class="[
+        v-else
+        :class="[
         'carsList-itemContent',
         [carousel ? 'carsList-itemContent-agile' : ''],
       ]"
     >
       <CarModelInfo :car="car" :carousel="carousel" />
+
+      <!-- Слой оверлея, который для .is-bought-out будет виден всегда -->
+      <div class="carsList-itemHover" v-if="car.is_bought_out === 1">
+        <div class="carsList-itemButtonContainer">
+          <div class="carsList-itemButton btn-bought-out">Выкуплено</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -64,7 +74,24 @@ export default {
   height: 260px;
 }
 
-.agile__slides > * {
-  /* margin: 0 10px; */
+/* Принудительно показываем оверлей (как при ховере) для выкупленных авто */
+.is-bought-out .carsList-itemHover {
+  opacity: 1 !important;
+  visibility: visible !important;
+  cursor: default !important;
+}
+
+/* Серая кнопка без заливки, один-в-один как оригинал */
+.btn-bought-out {
+  border-color: #888 !important;
+  color: #888 !important;
+  background: transparent !important;
+  pointer-events: none;
+}
+
+/* Стандартное поведение ховера для активных карточек */
+.carsList-itemContent:hover .carsList-itemHover {
+  opacity: 1;
+  visibility: visible;
 }
 </style>
